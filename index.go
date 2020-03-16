8cb14d7ed65f532882e738e038adfce44563232d
codeallthethingz <howdoicontactwill@gmail.com>
2020-03-16 06:50:28 -0700

    initial commit

---
+++ index.go
@@ -0,0 +1,107 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/nlopes/slack"
+	"github.com/simpleforce/simpleforce"
+)
+
+var client *simpleforce.Client
+
+// Handler - check routing and call correct methods
+func Handler(w http.ResponseWriter, r *http.Request) {
+	slackVerificationCode, sfURL, sfUser, sfPassword, sfToken, err := getEnvironmentValues()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	s, err := slack.SlashCommandParse(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	if !s.ValidateToken(slackVerificationCode) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if client == nil {
+		client = simpleforce.NewClient(sfURL, simpleforce.DefaultClientID, simpleforce.DefaultAPIVersion)
+	}
+	if client == nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	err = client.LoginPassword(sfUser, sfPassword, sfToken)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	switch s.Command {
+	case "/rep":
+		rep, err := getRep(s.Text)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Header().Set("Content-type", "application/json")
+		w.Write([]byte(`{
+			"response_type": "in_channel",
+			"text": "Rep: ` + rep + `"
+		}`))
+
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func getRep(repName string) (string, error) {
+	// TODO sanatize input to prevent accidental SQL injection from people in our slack.
+	q := "SELECT Name FROM User WHERE id IN (SELECT CS_Manager__c FROM Account WHERE Website ='" + repName + "')"
+	result, err := client.Query(q)
+	if err != nil {
+		return "", err
+	}
+
+	name := "unknown"
+	for _, record := range result.Records {
+		name = fmt.Sprintf("%s", record["Name"])
+	}
+	return name, nil
+}
+
+func getEnvironmentValues() (string, string, string, string, string, error) {
+	if os.Getenv("SLACK_VERIFICATION_TOKEN") == "" {
+		return "", "", "", "", "", fmt.Errorf("Must set: SLACK_VERIFICATION_TOKEN")
+	}
+	if os.Getenv("SF_URL") == "" {
+		return "", "", "", "", "", fmt.Errorf("Must set: SF_URL")
+	}
+	if os.Getenv("SF_USER") == "" {
+		return "", "", "", "", "", fmt.Errorf("Must set: SF_USER")
+	}
+	if os.Getenv("SF_PASSWORD") == "" {
+		return "", "", "", "", "", fmt.Errorf("Must set: SF_PASSWORD")
+	}
+	if os.Getenv("SF_TOKEN") == "" {
+		return "", "", "", "", "", fmt.Errorf("Must set: SF_TOKEN")
+	}
+	return os.Getenv("SLACK_VERIFICATION_TOKEN"),
+		os.Getenv("SF_URL"),
+		os.Getenv("SF_USER"),
+		os.Getenv("SF_PASSWORD"),
+		os.Getenv("SF_TOKEN"), nil
+}
